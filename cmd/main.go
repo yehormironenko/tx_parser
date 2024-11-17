e8665cca1ae884bb1a9ff3386615b0e0d1104448
Yahor Mironenka <yahor.mironenka@finteqhub.com>
2024-11-17 23:58:51 +0100

    Added gracefull shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/yehormironenko/tx_parser/cmd/initialize"
 )
@@ -20,9 +26,37 @@ func main() {
 	go components.NotificationService.ProcessNotifications()
 
 	// Start HTTP server
-	err = http.ListenAndServe(fmt.Sprintf("%v:%v", components.Config.Server.Host, components.Config.Server.Port), components.Mux)
-	if err != nil {
-		components.Logger.Fatalf("Server failed to start: %v", err)
+	server := &http.Server{
+		Addr:    fmt.Sprintf("%v:%v", components.Config.Server.Host, components.Config.Server.Port),
+		Handler: components.Mux,
 	}
-	components.Logger.Printf("Server started on %v:%v", components.Config.Server.Host, components.Config.Server.Port)
+
+	go func() {
+		components.Logger.Printf("Server started on %v:%v", components.Config.Server.Host, components.Config.Server.Port)
+		if err = server.ListenAndServe(); err != nil && !errors.Is(http.ErrServerClosed, err) {
+			components.Logger.Fatalf("Server failed to start: %v", err)
+		}
+	}()
+
+	gracefulShutdown(server, components.Logger)
+}
+
+func gracefulShutdown(server *http.Server, logger *log.Logger) {
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until a signal is received
+	<-stopChan
+	logger.Println("Shutting down server...")
+
+	// Create a context with a timeout for the shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Attempt graceful shutdown
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	logger.Println("Server gracefully stopped")
 }
